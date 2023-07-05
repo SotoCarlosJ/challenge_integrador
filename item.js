9729328949a5a73b739ec62ef5516f4f91ec82c1
Carlos Soto <carloss.empire@gmail.com>
2023-07-05 11:32:17 -0300

    Cambios en codigo CSS

--- item.js
+++
@@ -1,6 +0,0 @@
-const btnRemove = document.getElementById('btn_remove');
-const btnAdd = document.getElementById('btn_add');
-const itemQty = document.getElementById('item_qty');
-
-btnRemove.addEventListener('click', () => itemQty.value == 0 ? itemQty.value = itemQty.value : itemQty.value = parseInt(itemQty.value) - 1);
-btnAdd.addEventListener('click', () => itemQty.value = parseInt(itemQty.value) + 1);
